a619dbf5fc2c8e3475d119b64db0baeb2f468d34
Pavlina Pavlova <pavlina.pavlova@gmail.com>
2015-11-16 15:23:18 +0100

    initial project creation

---
+++ CustomAnimation/DemoAnimationActivity.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CustomAnimation
+{
+	[Activity (Label = "DemoAnimationActivity")]			
+	public class DemoAnimationActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Demo);
+		}
+	}
+}
---
+++ CustomAnimation/ViewAnimationActivity.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CustomAnimation
+{
+	[Activity (Label = "ViewAnimationActivity")]			
+	public class ViewAnimationActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.View);
+		}
+	}
+}
